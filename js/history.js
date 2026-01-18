ac15d1343a3c3ae42f513fee07ff8dbb3d208abd
AzSora09 <azaanal09@gmail.com>
2026-01-18 23:09:25 +0500

    Navbar finished (?)

--- js/history.js
+++ js/history.js
@@ -1,9 +1,9 @@
 $(window).scroll(function () {
     var scroll = $(window).scrollTop();
-    if ($(window).scrollTop() > 400) {
+    if ($(window).scrollTop() > 200) {
         $("#navEl").addClass("scrolldown fixed-top mt-2 mx-auto")
     }
-    else if ($(window).scrollTop() < 400) {
+    else if ($(window).scrollTop() < 200) {
         $("#navEl").removeClass("scrolldown fixed-top mt-2 mx-auto")
     }
     else { 
