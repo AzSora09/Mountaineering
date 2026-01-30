3a141047b9da743cec2c05b173bee24c8b2f6082
AzSora09 <azaanal09@gmail.com>
2026-01-30 08:28:12 +0500

    Home done, Clubs and Organizations remaining

--- js/base.js
+++ js/base.js
@@ -2,10 +2,10 @@
 $(window).scroll(function () {
     var scroll = $(window).scrollTop();
     if ($(window).scrollTop() > 200) {
-        $("#navEl").addClass("scrolldown fixed-top mt-2 mx-auto")
+        $("#navEl").addClass("scrolldown fixed-top mt-2 mx-auto");
     }
     else if ($(window).scrollTop() < 200) {
-        $("#navEl").removeClass("scrolldown fixed-top mt-2 mx-auto")
+        $("#navEl").removeClass("scrolldown fixed-top mt-2 mx-auto");
     }
     else { 
     }
---
+++ js/home.js
@@ -0,0 +1,19 @@
+$("#historyBtn").click(function(){
+    window.location.href="./history.html";
+    return false;
+})
+
+$("#storyBtn").click(function(){
+    window.location.href="./success-stories.html";
+    return false;
+})
+
+$("#galleryBtn").click(function(){
+    window.location.href="./gallery.html";
+    return false;
+})
+
+$("#clubBtn").click(function(){
+    window.location.href="./clubs-org.html";
+    return false;
+})
